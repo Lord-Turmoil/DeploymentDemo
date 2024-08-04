65cd27128ff41254ce4ef9bbeac144e624c38b11
Lord Turmoil <tony-turmoil@outlook.com>
2024-08-04 16:07:04 +0800

    init project

---
+++ src/main/java/top/tony/spring/deployment/Controller.java
@@ -0,0 +1,14 @@
+package top.tony.spring.deployment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class Controller {
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
+}
